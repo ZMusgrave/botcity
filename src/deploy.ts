03ed449f4d0c9d0c4596a294d0866ac8dd9f3669
Zach Musgrave <musgrave.zm99@gmail.com>
2024-05-12 21:36:18 -0700

    base deploy.ts file - api error

---
+++ src/deploy.ts
@@ -0,0 +1,70 @@
+import { REST, Routes } from 'discord.js';
+import dotenv from 'dotenv';
+import fs from 'node:fs';
+
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+dotenv.config();
+
+const clientId = process.env.CLIENT_ID as string;
+const guildId = process.env.GUILD_ID as string;
+const token = process.env.TOKEN as string;
+
+async function deploy() {
+  const commands = [];
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  // Grab all the command folders from the commands directory you created earlier
+  const foldersPath = path.join(__dirname, 'commands');
+  const commandFolders = fs.readdirSync(foldersPath);
+
+  for (const folder of commandFolders) {
+    // Grab all the command files from the commands directory you created earlier
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs
+      .readdirSync(commandsPath)
+      .filter((file) => file.endsWith('.js'));
+    // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = await import(filePath);
+      if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+      } else {
+        console.log(
+          `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+        );
+      }
+    }
+  }
+
+  // Construct and prepare an instance of the REST module
+  const rest = new REST().setToken(token);
+
+  // and deploy your commands!
+  (async () => {
+    try {
+      console.log(
+        `Started refreshing ${commands.length} application (/) commands.`
+      );
+
+      // The put method is used to fully refresh all commands in the guild with the current set
+      // TODO: Find type for this ZMONEY
+      const data: any = await rest.put(
+        Routes.applicationGuildCommands(clientId, guildId),
+        { body: commands }
+      );
+
+      console.log(
+        `Successfully reloaded ${data.length} application (/) commands.`
+      );
+    } catch (error) {
+      // And of course, make sure you catch and log any errors!
+      console.error(error);
+    }
+  })();
+}
+
+deploy();
