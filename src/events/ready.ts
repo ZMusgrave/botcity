d04ada4403d50aef81d1288c78ba4d4e18b29819
Zach Musgrave <musgrave.zm99@gmail.com>
2024-05-13 22:00:36 -0700

    create ready event file

---
+++ src/events/ready.ts
@@ -0,0 +1,14 @@
+import { Events, Client } from 'discord.js';
+import { ReadyEvent } from '../../types/event';
+
+async function execute(client: Client) {
+  console.log(`Ready! Logged in as ${client?.user?.tag}`);
+}
+
+const event: ReadyEvent = {
+  name: Events.ClientReady,
+  once: true,
+  execute,
+};
+
+export default event;
