e14dcabf253d20e882a81525b85687c418f95707
Zach Musgrave <musgrave.zm99@gmail.com>
2024-05-14 13:20:40 -0700

    instantiate new cooldowns collection on bot

--- src/bot.ts
+++ src/bot.ts
@@ -15,6 +15,7 @@ async function Bot() {
   }) as ClientWithCommands;
 
   client.commands = new Collection();
+  client.cooldowns = new Collection();
 
   const foldersPath = path.join(__dirname, 'commands');
   const commandFolders = fs.readdirSync(foldersPath);
