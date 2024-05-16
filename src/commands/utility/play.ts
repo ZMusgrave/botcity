fc63336cc91ebcef03463c01efbbaf08515f5c75
Zach Musgrave <musgrave.zm99@gmail.com>
2024-05-15 17:16:06 -0700

    instantiate base player config with commands

--- src/commands/utility/play.ts
+++ src/commands/utility/play.ts
@@ -3,13 +3,40 @@ import { Player } from 'discord-player';
 
 const data = new SlashCommandBuilder()
   .setName('play')
-  .setDescription('Allows posting songs for youtube or spotify');
+  .setDescription('Allows posting songs for youtube or spotify')
+  .addStringOption((option) =>
+    option
+      .setName('music')
+      .setDescription('Where the music will play from Spotify Or Youtube')
+      .setRequired(true)
+      .addChoices(
+        {
+          name: 'Spotify',
+          value: 'spotify',
+        },
+        {
+          name: 'Youtube',
+          value: 'youtube',
+        }
+      )
+  );
 
 async function execute(interaction: CommandInteraction) {
   const player = new Player(interaction.client);
-  console.log(player);
+
+  await player.extractors.loadDefault((ext) => ext !== 'YouTubeExtractor');
+
+  player.events.on('playerStart', (queue, track) => {
+    // we will later define queue.metadata object while creating the queue
+    queue.metadata.channel.send(`Started playing **${track.title}**!`);
+  });
+
+  player.on('error', (error) => {
+    console.log(`Error emitted from the queue: ${error.message}`);
+  });
+
   await interaction.reply(
-    `This server is ${interaction.guild?.name} and has ${interaction.guild?.memberCount} members.`
+    `Play Request Received, your song is currently #3 in the queue`
   );
 }
 
